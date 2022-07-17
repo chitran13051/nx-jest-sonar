aa68fdd7a3a49e6ce2cae26e1a2bfb4077528cfc
Chi Tran <khanhchi.tran13051@gmail.com>
2022-07-17 08:56:03 +0200

    is CodeSmell?

--- packages/say-good-bye/src/lib/say-good-bye.ts
+++ packages/say-good-bye/src/lib/say-good-bye.ts
@@ -1,4 +1,8 @@
 export function sayGoodBye(): string {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  // eslint-disable-next-line no-constant-condition
+  if (4 === 6) return 1525;
   return 'say-good-bye';
 }
 
