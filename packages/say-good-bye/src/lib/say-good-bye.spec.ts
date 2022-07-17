eb4349d434ce35b0e2a66007f55b7e9b7fc45ec2
Chi Tran <khanhchi.tran13051@gmail.com>
2022-07-17 08:39:31 +0200

    retrigger analysic

--- packages/say-good-bye/src/lib/say-good-bye.spec.ts
+++ packages/say-good-bye/src/lib/say-good-bye.spec.ts
@@ -7,4 +7,8 @@ describe('sayGoodBye', () => {
   it('dummy test', () => {
     expect('hello').toBe('hello');
   });
+  it('dummy test 2', () => {
+    let dummyVar;
+    expect('hello1').toBe('hello1');
+  });
 });
