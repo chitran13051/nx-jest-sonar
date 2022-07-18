11162bce5c6811845e25ae034b951d5eb8683760
Chi Tran <khanhchi.tran13051@gmail.com>
2022-07-18 23:38:01 +0200

    add new test

--- packages/mul/src/lib/mul.spec.ts
+++ packages/mul/src/lib/mul.spec.ts
@@ -1,7 +1,13 @@
-import { mul } from './mul';
+import { greaterThanTen, mul } from './mul';
 
 describe('mul', () => {
   it('should work', () => {
     expect(mul(6, 8)).toEqual(48);
+  });
+});
+
+describe('greateThanTen', () => {
+  it('should return `true`', () => {
+    expect(greaterThanTen(14)).toBe(true);
   });
 });
--- packages/mul/src/lib/mul.ts
+++ packages/mul/src/lib/mul.ts
@@ -1,3 +1,5 @@
 export function mul(a: number, b: number): number {
   return a * b;
 }
+
+export const greaterThanTen = (a: number): boolean => a >= 10;
